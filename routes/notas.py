d138368bb9bc903d2658987a6ba198d98275f1d8
AndressaSales <andressaccs2001@gmail.com>
2024-09-03 17:32:34 -0300

    atualizando os formularios

--- routes/notas.py
+++ routes/notas.py
@@ -1,20 +1,27 @@
 from flask import Blueprint ,render_template, request
 #from database.models.blocoNotas import bancoNotas
-
+from database.bloco import blocos
 notas_routes = Blueprint('notas', __name__)
 
 @notas_routes.route('/')
 def postados():
-    blocos = [
-        {"title": "Fazer tarefa", "text": "Preciso fazer as tarrefas de matematica e portugues"},
-        {"title": "Fazer tarefa", "text": "Preciso fazer as tarrefas de matematica e portugues"},
-        {"title": "Fazer tarefa", "text": "Preciso fazer as tarrefas de matematica e portugues"},
-        {"title": "Fazer tarefa", "text": "Preciso fazer as tarrefas de matematica e portugues"},
-        {"title": "Fazer tarefa", "text": "Preciso fazer as tarrefas de matematica e portugues"},
-        {"title": "Fazer tarefa", "text": "Preciso fazer as tarrefas de matematica e portugues"},
-    ]
     return render_template('lista.html', blocos=blocos)
+
+
+@notas_routes.route('/new')
+def formulario():
+    """ formulario para cadastrar um cliente """
+    return render_template('formulario.html')
+
 
 @notas_routes.route('/', methods=['POST'])
 def inserir_notas():
-    return render_template('form.html')
+    """ inserir anotações novas """
+    data = request.json
+
+    nova_anotacao = blocos.create(
+        title = data['title'],
+        text = data['text']
+    )
+
+    return render_template('items.html', notas=nova_anotacao)
